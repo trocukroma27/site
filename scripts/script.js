68f714ccaaea4e0e63fa8f84b8310eb7a5a3589b
trocukroma27 <trocukroma@gmail.com>
2021-02-25 18:08:20 +0200

    1st commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+function slowScroll(id){
+    var offset = 50;
+    $('html, body').animate({
+        scrollTop: $(id).offset().top - offset
+    }, 500);
+    return false
+}
