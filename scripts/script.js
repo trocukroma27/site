abf911704fa97e43c7d29748954316d517edcb7f
trocukroma27 <trocukroma@gmail.com>
2021-02-27 15:43:50 +0200

    End of layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.header__burger').click(function(event){
+    $('.header__burger').click(function(){
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
